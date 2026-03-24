29ad4a14e1de746094bfc12a20e337240a8b3f94
Dante Chavero <dantechavero1@gmail.com>
2026-03-24 01:46:48 -0300

    Agrega rondas con todas las palabras de la categoria seleccionada sin repeticiones, tambien agrega un contador global

--- questions.py
+++ questions.py
@@ -8,6 +8,8 @@ categorias= {
 
 lista_categorias= list(categorias.keys())
 
+print("Categorias: ")
+
 #Imprime cada categoria con un numero para seleccionar
 for i in range(len(lista_categorias)):
     print(f"{i+1} - {lista_categorias[i]}")
@@ -22,61 +24,70 @@ while True:
 
 eleccion= lista_categorias[numero-1]
 
-word = random.choice(categorias[eleccion])
-guessed = []
-attempts = 6
+#Agarra todas las palabras de la categoria seleccionada en un orden aleatorio
+palabras_mezcladas=random.sample(categorias[eleccion], len(categorias[eleccion]))
 
+puntaje_total=0
 print("¡Bienvenido al Ahorcado!")
-print()
 
-puntaje=0
+for word in palabras_mezcladas:
+    guessed = []
+    attempts = 6
 
-while attempts > 0:
-    # Mostrar progreso: letras adivinadas y guiones para las que faltan
-    progress = ""
-    for letter in word:
-
-        #Agregue esta validacion ya que algunas palabras que puse tenian espacios
-        if letter == " ":
-            progress+= "  "
-        elif letter in guessed:
-            progress += letter + " "
-        else:
-            progress += "_ "
-    print(progress)
-    # Verificar si el jugador ya adivinó la palabra completa
-    if "_" not in progress:
-        print("¡Ganaste!")
-        puntaje+=6
-        break
-    
-    print(f"Intentos restantes: {attempts}")
-    print(f"Letras usadas: {', '.join(guessed)}")
-    
-    #Agrego el lower para que se puedan poner mayusculas
-    letter = input("Ingresá una letra: ").lower()
-    
-    #Verifica que sea una letra y que no haya mas de una
-    if len(letter)!=1 or not letter.isalpha():    
-        print("Entrada no valida")
-        print()
-        continue
-    
-    if letter in guessed:
-        print("Ya usaste esa letra.")
-    elif letter in word:
-        guessed.append(letter)
-        print("¡Bien! Esa letra está en la palabra.")
-    else:
-        guessed.append(letter)
-        attempts -= 1
-        puntaje-=1
-        print("Esa letra no está en la palabra.")
-    
     print()
 
-else:
-    print(f"¡Perdiste! La palabra era: {word}")
     puntaje=0
 
-print(f"Puntaje conseguido: {puntaje}")
+    while attempts > 0:
+        # Mostrar progreso: letras adivinadas y guiones para las que faltan
+        progress = ""
+        for letter in word:
+
+            #Agregue esta validacion ya que algunas palabras que puse tenian espacios
+            if letter == " ":
+                progress+= "  "
+            elif letter in guessed:
+                progress += letter + " "
+            else:
+                progress += "_ "
+        print(progress)
+        # Verificar si el jugador ya adivinó la palabra completa
+        if "_" not in progress:
+            print("¡Ganaste!")
+            puntaje+=6
+            break
+    
+        print(f"Intentos restantes: {attempts}")
+        print(f"Letras usadas: {', '.join(guessed)}")
+    
+        #Agrego el lower para que se puedan poner mayusculas
+        letter = input("Ingresá una letra: ").lower()
+    
+        #Verifica que sea una letra y que no haya mas de una
+        if len(letter)!=1 or not letter.isalpha():    
+            print("Entrada no valida")
+            print()
+            continue
+    
+        if letter in guessed:
+            print("Ya usaste esa letra.")
+        elif letter in word:
+            guessed.append(letter)
+            print("¡Bien! Esa letra está en la palabra.")
+        else:
+            guessed.append(letter)
+            attempts -= 1
+            puntaje-=1
+            print("Esa letra no está en la palabra.")
+    
+        print()
+
+    else:
+        print(f"¡Perdiste! La palabra era: {word}")
+        puntaje=0
+    
+    print(f"Puntaje de ronda conseguido: {puntaje}")
+    puntaje_total+=puntaje
+    print()
+
+print(f"Puntaje total conseguido: {puntaje_total}")
