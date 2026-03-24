4a22a9a9a10032e81e09b325b149ddced1950c32
Dante Chavero <dantechavero1@gmail.com>
2026-03-23 23:28:36 -0300

    Agrega verificacion de que la letra sea valida

--- questions.py
+++ questions.py
@@ -24,8 +24,8 @@ while attempts > 0:
     for letter in word:
         if letter in guessed:
             progress += letter + " "
-    else:
-        progress += "_ "
+        else:
+            progress += "_ "
     print(progress)
     # Verificar si el jugador ya adivinó la palabra completa
     if "_" not in progress:
@@ -36,7 +36,12 @@ while attempts > 0:
     print(f"Letras usadas: {', '.join(guessed)}")
     
     letter = input("Ingresá una letra: ")
-
+    #Verifica que sea una letra y que no haya mas de una
+    if len(letter)!=1 or not letter.isalpha():    
+        print("Entrada no valida")
+        print()
+        continue
+    
     if letter in guessed:
         print("Ya usaste esa letra.")
     elif letter in word:
